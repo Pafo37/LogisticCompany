a30c400a6401c3b99e7d1a69564173e6fa416154
Pafo37 <pafo96@mail.bg>
2025-01-28 18:49:32 +0200

    Add basic UI using thymeleaf for office

--- src/main/java/com/logisticcompany/controller/OfficeController.java
+++ src/main/java/com/logisticcompany/controller/OfficeController.java
@@ -3,26 +3,52 @@ package com.logisticcompany.controller;
 import com.logisticcompany.data.entity.Office;
 import com.logisticcompany.service.office.OfficeService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController
+@Controller
+@RequestMapping("/offices")
 @AllArgsConstructor
 public class OfficeController {
 
     private OfficeService officeService;
 
-    @GetMapping(value = "api/offices")
-    public List<Office> getAllOffices() {
-        return officeService.getAllOffices();
+    @GetMapping
+    public String getAllOffices(Model model) {
+        model.addAttribute("offices", officeService.getAllOffices());
+        return "offices";
     }
 
-    @GetMapping("api/offices/{id}")
-    public Office getOfficeById(@PathVariable Long id) {
-        return officeService.getOfficeById(id);
+    @GetMapping("/add")
+    public String showAddOfficeForm(Model model) {
+        model.addAttribute("office", new Office());
+        return "add_office";
+    }
+
+    @PostMapping("/add")
+    public String addOffice(@ModelAttribute Office office) {
+        officeService.saveOffice(office);
+        return "redirect:/offices";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditOfficeForm(@PathVariable Long id, Model model) {
+        model.addAttribute("office", officeService.getOfficeById(id));
+        return "edit_office";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editOffice(@PathVariable Long id, @ModelAttribute Office office) {
+        office.setId(id);
+        officeService.saveOffice(office);
+        return "redirect:/offices";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteOffice(@PathVariable Long id) {
+        officeService.deleteOffice(id);
+        return "redirect:/offices";
     }
 
 }
